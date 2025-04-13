7703f672ac0b6698c03b5a36266091805a562bf0
ladydanicorn <dbronsonart@gmail.com>
2025-04-13 15:36:32 -0400

    tweaked header

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,20 @@ export default {
   content: ['./src/**/*.{astro,html,js,ts,jsx,tsx,md,mdx}'],
   theme: {
     extend: {
+      colors: {
+        blush: '#F9E9E7',
+        sage: '#D6E6D6',
+        dusk: '#4A3F55',
+        rose: '#EA6974',
+      },
       fontFamily: {
+        serif: ['"Playfair Display"', 'serif'],
         sans: ['Inter', 'sans-serif'],
+      },
+      borderRadius: {
+        lg: '1rem',
+        xl: '1.5rem',
       },
     },
   }
-  ,
-  plugins: [],
-}
+};  
